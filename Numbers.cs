17829e0703515c0664e792830dfead8b2d170b10
nabellaag <nabellaayug@students.amikom.ac.id>
2022-04-07 15:43:35 +0700

    Method

---
+++ Numbers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    public class Numbers
+    {
+        private int _b, _e;
+
+        // menggunakan static method
+        public static int FindMinimum(int number1, int number2)
+        {
+            return number1 < number2 ? number1 : number2; //menggunakan operator ternary agar lebih praktis
+        }
+
+        // menggunakan overloading method
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            _b = number1 < number3 ? number1 : (int)number3;
+            _e = number2 < number3 ? number2 : (int)number3;
+            return number1 < number2 ? _b : _e;
+        }
+
+        // menggunakan static method
+        public static int FindMaximum(int number1, int number2)
+        {
+            return number1 > number2 ? number1 : number2;
+        }
+
+        // menggunakan overloading method
+        public int FindMaximum(int number1, int number2,float number3)
+        {
+            _b = number1 > number3 ? number1 : (int)number3;
+            _e = number2 > number3 ? number2 : (int)number3;
+            return number1 > number2 ? _b : _e;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LatihanOverloadingMethod // project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers number = new Numbers() ;
+
+            Console.WriteLine("Minimum #1: {0}", Numbers.FindMinimum(3,1));  //memanggil static method
+            Console.WriteLine("Minimum #2: {0}", number.FindMinimum(3, 2, 4));  //memanggil overloading method
+            Console.WriteLine();
+            Console.WriteLine("Maksimum #1: {0}", Numbers.FindMaximum(3, 1)); //memanggil static method
+            Console.WriteLine("Maksimum #2: {0}", number.FindMaximum(3, 2, 4)); //memanggil overloading method
+
+            Console.ReadKey();
+        }
+    }
+}
